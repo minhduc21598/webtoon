3d4e2a85473c2366ef923f982ccc0a4dec84cee9
moechino0412 <hieunt1998@gmail.com>
2019-06-29 17:38:10 +0700

    add screen My

---
+++ src/component/Data.js
@@ -0,0 +1,17 @@
+export const recentDataScreenMy = [
+    {
+        uri: 'http://truyenvip.net/content/upload/images/komi-san-wa-komyusho-desu.jpg',
+        title: 'Komi-san wa Komyushou desu',
+        rank: '#1'
+    },
+    {
+        uri: 'https://i.pinimg.com/736x/d3/78/5b/d3785b1b8a8074ebb116ef50bdab2f03.jpg',
+        title: 'Go-Toubun no Hanayome',
+        rank: '#2'
+    },
+    {
+        uri: 'https://www.thichtruyen.net/wp-content/uploads/2019/01/solo-leveling-novel.jpg',
+        title: 'Solo Leveling',
+        rank: '#3'
+    }
+]
