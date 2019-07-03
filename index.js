d0ede4bbb874c836c3dd665a87f4cf5f0199518e
moechino0412 <hieunt1998@gmail.com>
2019-07-03 15:14:50 +0700

    van chua co onRefresh va LoadMore

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import RootNav from './RootNav';
 import {name as appName} from './app.json';
 import Originals from './src/screen/Originals';
+import Canvas from './src/screen/Canvas';
 
-AppRegistry.registerComponent(appName, () => Originals);
+AppRegistry.registerComponent(appName, () => RootNav);
