f714aaac09eab08341eba633287892f54cac31ec
Minh Đức <ducpro21598@gmail.com>
2019-07-10 16:29:35 +0700

    Updated

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-/**
- * @format
- */
-import React from 'react';
 import { AppRegistry } from 'react-native';
 import RootNav from './RootNav';
 import { name as appName } from './app.json';
-import GenresOri from './src/screen/GenresOri';
-import App_Redux from './App_Redux';
-import { Provider } from 'react-redux';
-import store from './src_demo_Redux/stores/stores';
-
-// const App = () => (
-//     <Provider
-//         store={store}
-//     >
-//         <App_Redux />
-//     </Provider>
-// )
 
 AppRegistry.registerComponent(appName, () => RootNav)
