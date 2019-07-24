b914541762532ae94920b0c6fb87fbd270a452e3
Minh Đức <ducpro21598@gmail.com>
2019-07-24 17:01:15 +0700

    Updated Ranking

--- src/const.js
+++ src/const.js
@@ -1,3 +1,49 @@
 export const tabNameInDaily = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
-export const dataAnime = ['Airing', 'Upcoming', 'TV', 'Movie', 'Ova', 'Special'];
+export const dataAnime = ['TV', 'Movie', 'Ova', 'Special'];
 export const dataManga = ['Manga', 'Novels', 'Oneshots', 'Doujin', 'Manhwa', 'Manhua'];
+
+export const typeAnime = [
+    {
+        icon: 'television-classic',
+        name: 'TV'
+    },
+    {
+        icon: 'movie-roll',
+        name: 'Movie'
+    },
+    {
+        icon: 'snapchat',
+        name: 'Ova'
+    },
+    {
+        icon: 'star-half',
+        name: 'Special'
+    },
+];
+
+export const typeManga = [
+    {
+        icon: 'cat',
+        name: 'Manga'
+    },
+    {
+        icon: 'book',
+        name: 'Novels'
+    },
+    {
+        icon: 'crosshairs-gps',
+        name: 'Oneshots'
+    },
+    {
+        icon: 'github-face',
+        name: 'Doujin'
+    },
+    {
+        icon: 'star-face',
+        name: 'Manhwa'
+    },
+    {
+        icon: 'dog',
+        name: 'Manhua'
+    },
+];
