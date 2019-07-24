b7f5d2e58e39a15589eadddb3b6538e49a539e50
moechino0412 <hieunt1998@gmail.com>
2019-07-24 15:04:49 +0700

    xoa daily1

--- src/screen/Daily.js
+++ src/screen/Daily.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, StatusBar } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Icon1 from 'react-native-vector-icons/FontAwesome5';
-import GenresOri from './GenresOri';
-import Daily1 from './Daily1';
+import ScrollableTabView, { ScrollableTabBar, } from 'react-native-scrollable-tab-view';
+import ViewInScrollableTabView from '../component/ViewInScrollableTabView';
+import { tabNameInDaily } from '../const';
+import { dataOriginal } from '../component/Data';
 
 class Originals extends Component {
   constructor(props) {
@@ -13,7 +15,6 @@ class Originals extends Component {
     let colorGenres = (this.props.navigation.state.params === undefined) ? 'gray' : this.props.navigation.state.params.colorGenres;
     this.state = {
       isLoadingMore: false,
-      daily: daily,
       colorDaily: colorDaily,
       colorGenres: colorGenres,
     };
@@ -24,7 +25,7 @@ class Originals extends Component {
     let daily = (this.props.navigation.state.params === undefined) ? true : this.props.navigation.state.params.daily;
     let colorDaily = (this.props.navigation.state.params === undefined) ? 'black' : this.props.navigation.state.params.colorDaily;
     let colorGenres = (this.props.navigation.state.params === undefined) ? 'gray' : this.props.navigation.state.params.colorGenres;
-    this.setState({daily: daily, colorDaily: colorDaily, colorGenres: colorGenres});
+    this.setState({ daily: daily, colorDaily: colorDaily, colorGenres: colorGenres });
   }
 
   onPressDaily = () => {
@@ -45,33 +46,8 @@ class Originals extends Component {
           barStyle='dark-content'
         />
         <View style={styles.headerContainer}>
-          <View style={{ flexDirection: 'row' }}>
-            <TouchableOpacity
-              style={{ alignItems: 'center' }}
-              activeOpacity={1}
-              onPress = {this.onPressDaily}
-            >
-              <Text style={[styles.txtHeader, { color: this.state.colorDaily }]}>Daily</Text>
-            </TouchableOpacity>
-            <Text style={styles.txtHeader}>|</Text>
-            <TouchableOpacity
-              style={{
-                alignItems: 'center'
-              }}
-              activeOpacity={1}
-              onPress = {this.onPressGenres}
-            >
-              <Text style={[styles.txtHeader, { color: this.state.colorGenres }]}>Genres</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={{flexDirection: 'row'}}>
-          <TouchableOpacity
-            onPress={() => alert("btn Medal")}
-            activeOpacity={1}
-            style={{ marginRight: 25 }}
-          >
-            <Icon1 name='medal' size={23} color={'black'} />
-          </TouchableOpacity>
+
+          <Text style={[styles.txtHeader, { color: 'black' }]}>Daily</Text>
           <TouchableOpacity
             onPress={() => alert("btn Search")}
             activeOpacity={1}
@@ -79,16 +55,31 @@ class Originals extends Component {
           >
             <Icon name='ios-search' size={30} color={'black'} />
           </TouchableOpacity>
-          </View>
+
         </View>
-        {
-          (this.state.daily)
-            ? <Daily1/>
-            : <GenresOri 
-                ref = {ref => this.GenresOri = ref}
-                data = {index}
-              />
-        }
+        <ScrollableTabView
+          initialPage={0}
+          renderTabBar={() => <ScrollableTabBar style={{ borderTopWidth: 0.5, borderTopColor: '#d0cdcd' }} />}
+          tabBarInactiveTextColor={'gray'}
+          tabBarActiveTextColor={'black'}
+          tabBarUnderlineStyle={{ height: 2 }}
+        >
+          {
+            tabNameInDaily.map(
+              (item, index) => {
+                return (
+                  <ViewInScrollableTabView
+                    tabLabel={item}
+                    styleTxtCounter={styles.txtCounter}
+                    styleFlatGrid={styles.itemContainer}
+                    data={dataOriginal}
+                    key={index}
+                  />
+                )
+              }
+            )
+          }
+        </ScrollableTabView>
       </View>
     );
   }
@@ -101,7 +92,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 60,
     alignItems: 'center',
-    flexDirection: 'row', 
+    flexDirection: 'row',
     justifyContent: 'space-between'
   },
   txtHeader: {
@@ -110,4 +101,16 @@ const styles = StyleSheet.create({
     color: 'black',
     marginLeft: 18
   },
+  itemContainer: {
+    borderRadius: 5,
+    width: 100,
+    height: 160,
+  },
+  txtCounter: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 20,
+    marginTop: 10,
+    marginBottom: 10
+  }
 });
